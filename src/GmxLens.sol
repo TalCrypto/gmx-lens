e2ba89008efb4dd2626ba8fa0af55e92b14f6729
talcrypto <leehunter936@gmail.com>
2024-04-05 21:02:37 +0000

    feat: mvp

--- src/GmxLens.sol
+++ src/GmxLens.sol
@@ -3,6 +3,14 @@ pragma solidity 0.8.20;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {IReader} from "./gmx/reader/IReader.sol";
+import {IOracle} from "./gmx/oracle/IOracle.sol";
+import {IDataStore} from "./gmx/data/IDataStore.sol";
+import {Market} from "./gmx/market/Market.sol";
+import {MarketPoolValueInfo} from "./gmx/market/MarketPoolValueInfo.sol";
+import {MarketUtils} from "./gmx/market/MarketUtils.sol";
+import {ReaderUtils} from "./gmx/reader/ReaderUtils.sol";
+import {Keys} from "./gmx/data/Keys.sol";
 
 contract GmxLens is UUPSUpgradeable, OwnableUpgradeable {
     /// @custom:storage-location erc7201:logarithm.storage.gmxlens
@@ -40,16 +48,66 @@ contract GmxLens is UUPSUpgradeable, OwnableUpgradeable {
     }
 
     // keccak256(abi.encode(uint256(keccak256("logarithm.storage.gmxlens")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 private constant GmxLensStorageLocation =
-        0x46ddb7b117f47e3c543a4da0d79b5a9346fd0f9dececc4cd0df8c125c5135100;
+    bytes32 private constant GmxLensStorageLocation = 0x46ddb7b117f47e3c543a4da0d79b5a9346fd0f9dececc4cd0df8c125c5135100;
 
     function initialize(address gmxReader, address gmxDataStorage, address gmxOracle) public initializer {
         __Ownable_init(_msgSender());
         _setGmxLensStorage(GmxLensStorage({reader: gmxReader, dataStore: gmxDataStorage, oracle: gmxOracle}));
     }
 
+    /**
+     * @param marketID The address of market token
+     * @return state MarketDataState
+     */
     function getMarketData(address marketID) external view returns (MarketDataState memory state) {
+        (address reader, address dataStore, address oracle) = getGmxLensAddresses();
+        Market.Props memory market = IReader(reader).getMarket(dataStore, marketID);
 
+        state.marketToken = market.marketToken;
+        state.indexToken = market.indexToken;
+        state.longToken = market.longToken;
+        state.shortToken = market.shortToken;
+
+        MarketUtils.MarketPrices memory marketPrices = MarketUtils.getMarketPrices(IOracle(oracle), market);
+
+        bytes32 pnlFactorType = Keys.MAX_PNL_FACTOR_FOR_DEPOSITS;
+        bool maximize = true;
+
+        (, MarketPoolValueInfo.Props memory marketPoolValueInfo) = IReader(reader).getMarketTokenPrice(
+            dataStore,
+            market,
+            marketPrices.indexTokenPrice,
+            marketPrices.longTokenPrice,
+            marketPrices.shortTokenPrice,
+            pnlFactorType,
+            maximize
+        );
+
+        state.poolValue = uint256(marketPoolValueInfo.poolValue);
+        state.longTokenAmount = marketPoolValueInfo.longTokenAmount;
+        state.longTokenUsd = marketPoolValueInfo.longTokenUsd;
+        state.shortTokenAmount = marketPoolValueInfo.shortTokenAmount;
+        state.shortTokenUsd = marketPoolValueInfo.shortTokenUsd;
+        state.pnlLong = marketPoolValueInfo.longPnl;
+        state.pnlShort = marketPoolValueInfo.shortPnl;
+        state.netPnl = marketPoolValueInfo.netPnl;
+
+        state.openInterestLong = int256(MarketUtils.getOpenInterest(IDataStore(dataStore), market, true));
+        state.openInterestShort = int256(MarketUtils.getOpenInterest(IDataStore(dataStore), market, false));
+
+        ReaderUtils.MarketInfo memory marketInfo = IReader(reader).getMarketInfo(dataStore, marketPrices, marketID);
+
+        state.borrowingFactorPerSecondForLongs = marketInfo.borrowingFactorPerSecondForLongs;
+        state.borrowingFactorPerSecondForShorts = marketInfo.borrowingFactorPerSecondForShorts;
+        state.longsPayShorts = marketInfo.nextFunding.longsPayShorts;
+        state.fundingFactorPerSecond = marketInfo.nextFunding.fundingFactorPerSecond;
+        // state.fundingFactorPerSecondLongs => not exit
+        // state.fundingFactorPerSecondShorts => not exit
+
+        state.reservedUsdLong = MarketUtils.getReservedUsd(IDataStore(dataStore), market, marketPrices, true);
+        state.reservedUsdShort = MarketUtils.getReservedUsd(IDataStore(dataStore), market, marketPrices, false);
+        state.maxOpenInterestUsdLong = MarketUtils.getMaxOpenInterest(IDataStore(dataStore), market.marketToken, true);
+        state.maxOpenInterestUsdShort = MarketUtils.getMaxOpenInterest(IDataStore(dataStore), market.marketToken, false);        
     }
 
     function getGmxLensAddresses() public view returns (address reader, address dataStore, address oracle) {
