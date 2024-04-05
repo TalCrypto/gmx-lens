02d1e30618fa7e3edfa8b8c6487f21d158e2afb8
talcrypto <leehunter936@gmail.com>
2024-04-05 16:18:28 +0000

    feat: add dataStore address

--- src/GmxLens.sol
+++ src/GmxLens.sol
@@ -5,25 +5,32 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract GmxLens is UUPSUpgradeable, OwnableUpgradeable {
-
     /// @custom:storage-location erc7201:logarithm.storage.gmxreader
     struct GmxReaderStorage {
         address reader;
+        address dataStore;
     }
 
     // keccak256(abi.encode(uint256(keccak256("logarithm.storage.gmxreader")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 private constant GmxReaderStorageLocation = 0xbce1d4318a1e299492a97d978aca925117f372c83762806dec7145e898132200;
+    bytes32 private constant GmxReaderStorageLocation =
+        0xbce1d4318a1e299492a97d978aca925117f372c83762806dec7145e898132200;
 
-    function initialize(address _gmxReader) public initializer {
+    function initialize(address gmxReader, address gmxDataStorage) public initializer {
         __Ownable_init(_msgSender());
-        GmxReaderStorage storage $ = _getGmxReaderStorage();
-        $.reader = _gmxReader;
+        _setGmxReaderStorage(GmxReaderStorage({reader: gmxReader, dataStore: gmxDataStorage}));
     }
 
-    function gmxReader() public view returns (address) {
-        GmxReaderStorage storage $ = _getGmxReaderStorage();
-        return $.reader;
+    function setGmxReaderAddresses(address gmxReader, address gmxDataStorage) external onlyOwner {
+        _setGmxReaderStorage(GmxReaderStorage({reader: gmxReader, dataStore: gmxDataStorage}));
     }
+
+    function getGmxReaderAddresses() public view returns (address reader, address dataStore) {
+        GmxReaderStorage storage $ = _getGmxReaderStorage();
+        reader = $.reader;
+        dataStore = $.dataStore;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function _getGmxReaderStorage() private pure returns (GmxReaderStorage storage $) {
         assembly {
@@ -31,5 +38,9 @@ contract GmxLens is UUPSUpgradeable, OwnableUpgradeable {
         }
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    function _setGmxReaderStorage(GmxReaderStorage memory value) private {
+        GmxReaderStorage storage $ = _getGmxReaderStorage();
+        $.reader = value.reader;
+        $.dataStore = value.dataStore;
+    }
 }
